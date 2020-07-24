898b396bdd5a86b3ca9453e86dbf72287ea4ad1b
Josh Forisha <josh@forisha.com>
2020-07-24 12:36:31 -0500

    Setup Electron app for UI

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
-import Tone from "tone";
+const path = require("path");
+const url = require("url");
+const { BrowserWindow, app } = require("electron");
 
-console.log(Tone);
+let window = null;
 
-// const synth = new Tone.Synth().toMaster();
-// synth.triggerAttackRelease("C4", "8n");
+function createWindow() {
+  window = new BrowserWindow({ height: 800, width: 800 });
+  // window.webContents.openDevTools();
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, "./dist/index.html"),
+    protocol: "file:",
+    slashes: true,
+  });
+  window.loadURL(startUrl);
+
+  window.on("closed", () => {
+    window = null;
+  });
+}
+
+app.on("activate", () => {
+  if (window === null) {
+    createWindow();
+  }
+});
+
+app.on("ready", () => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
--- server/main.js
+++ server/main.js
@@ -10,7 +10,6 @@ const port = 5000;
 
 const discordClient = new Discord.Client();
 const noop = () => {};
-const wss = new WS.Server({ port });
 
 let conn;
 let dispatcher;
@@ -36,155 +35,157 @@ process.on("SIGINT", () => {
   process.exit(0);
 });
 
-wss.on("connection", (ws) => {
-  async function connectToVoiceChannel(channelId) {
-    const voiceChannel = discordClient.channels.cache
-      .array()
-      .find((channel) => channel.id === channelId);
+discordClient.on("ready", () => {
+  const wss = new WS.Server({ port });
 
-    try {
-      conn = await voiceChannel.join();
-    } catch (error) {
-      sendError(error);
-    }
+  wss.on("connection", (ws) => {
+    async function connectToVoiceChannel(channelId) {
+      const voiceChannel = discordClient.channels.cache
+        .array()
+        .find((channel) => channel.id === channelId);
 
-    sounds = fs
-      .readdirSync("sounds")
-      .filter((filename) => filename !== ".gitkeep")
-      .reduce((xs, filename) => {
-        const name = path.basename(filename, path.extname(filename));
-        xs[name] = filename;
-        return xs;
-      }, {});
-    state.sounds = Object.keys(sounds);
-
-    disconnect = () => {
-      console.log("Disconnecting from voice channel");
-      discordClient.user.setActivity(null);
-      conn.disconnect();
-      state.currentChannel = null;
-      send(state);
-      disconnect = noop;
-    };
-
-    state.currentChannel = {
-      iconUrl: voiceChannel.guild.iconURL(),
-      id: voiceChannel.id,
-      name: `${voiceChannel.guild.name} – ${voiceChannel.name}`,
-    };
-    send(state);
-  }
-
-  function playSound(soundName, update = true) {
-    dispatcher = conn
-      .play(`./sounds/${sounds[soundName]}`, {
-        volume: state.volume,
-      })
-      .on("finish", () => playSound(soundName, false));
-
-    if (update) {
-      state.currentMedia = soundName;
-      state.playing = true;
-      send(state);
-      discordClient.user.setActivity(state.currentMedia, { type: "PLAYING" });
-    }
-  }
-
-  async function playUrl(url, update = true) {
-    let title;
-    if (update) {
       try {
-        const info = await ytdl.getBasicInfo(url);
-        title = info.title;
+        conn = await voiceChannel.join();
       } catch (error) {
         sendError(error);
       }
-    }
 
-    let source;
-    try {
-      source = await ytdl(url, {
-        quality: "highestaudio",
-      });
-    } catch (error) {
-      sendError(error);
-    }
+      sounds = fs
+        .readdirSync("sounds")
+        .filter((filename) => filename !== ".gitkeep")
+        .reduce((xs, filename) => {
+          const name = path.basename(filename, path.extname(filename));
+          xs[name] = filename;
+          return xs;
+        }, {});
+      state.sounds = Object.keys(sounds);
 
-    dispatcher = conn
-      .play(source, { seek: 0, type: "opus", volume: state.volume })
-      .on("finish", () => playUrl(url, false));
+      disconnect = () => {
+        console.log("Disconnecting from voice channel");
+        discordClient.user.setActivity(null);
+        conn.disconnect();
+        state.currentChannel = null;
+        send(state);
+        disconnect = noop;
+      };
 
-    if (update) {
-      state.currentMedia = title;
-      state.playing = true;
+      state.currentChannel = {
+        iconUrl: voiceChannel.guild.iconURL(),
+        id: voiceChannel.id,
+        name: `${voiceChannel.guild.name} – ${voiceChannel.name}`,
+      };
       send(state);
-      discordClient.user.setActivity(state.currentMedia, { type: "PLAYING" });
     }
-  }
 
-  function togglePlay() {
-    if (state.playing) {
-      state.playing = false;
-      if (dispatcher) dispatcher.pause();
-    } else {
-      state.playing = true;
-      if (dispatcher) dispatcher.resume();
+    function playSound(soundName, update = true) {
+      dispatcher = conn
+        .play(`./sounds/${sounds[soundName]}`, {
+          volume: state.volume,
+        })
+        .on("finish", () => playSound(soundName, false));
+
+      if (update) {
+        state.currentMedia = soundName;
+        state.playing = true;
+        send(state);
+        discordClient.user.setActivity(state.currentMedia, { type: "PLAYING" });
+      }
     }
+
+    async function playUrl(url, update = true) {
+      let title;
+      if (update) {
+        try {
+          const info = await ytdl.getBasicInfo(url);
+          title = info.title;
+        } catch (error) {
+          sendError(error);
+        }
+      }
+
+      let source;
+      try {
+        source = await ytdl(url, {
+          quality: "highestaudio",
+        });
+      } catch (error) {
+        sendError(error);
+      }
+
+      dispatcher = conn
+        .play(source, { seek: 0, type: "opus", volume: state.volume })
+        .on("finish", () => playUrl(url, false));
+
+      if (update) {
+        state.currentMedia = title;
+        state.playing = true;
+        send(state);
+        discordClient.user.setActivity(state.currentMedia, { type: "PLAYING" });
+      }
+    }
+
+    function togglePlay() {
+      if (state.playing) {
+        state.playing = false;
+        if (dispatcher) dispatcher.pause();
+      } else {
+        state.playing = true;
+        if (dispatcher) dispatcher.resume();
+      }
+      send(state);
+    }
+
+    send = function (object) {
+      ws.send(JSON.stringify(object));
+    };
+
+    function sendError(error) {
+      console.error(error);
+      send({ error });
+    }
+
     send(state);
-  }
 
-  send = function (object) {
-    ws.send(JSON.stringify(object));
-  };
+    ws.on("close", () => {
+      console.log("WS Disconnected");
+      send = noop;
+    });
 
-  function sendError(error) {
-    console.error(error);
-    send({ error });
-  }
+    ws.on("message", (message) => {
+      const action = JSON.parse(message);
 
-  send(state);
+      switch (action.type) {
+        case "CONNECT_TO_CHANNEL":
+          connectToVoiceChannel(action.channelId);
+          break;
 
-  ws.on("close", () => {
-    console.log("WS Disconnected");
-    send = noop;
+        case "DISCONNECT":
+          disconnect();
+          break;
+
+        case "PLAY_SOUND":
+          playSound(action.sound);
+          break;
+
+        case "PLAY_URL":
+          playUrl(action.url);
+          break;
+
+        case "SET_VOLUME":
+          state.volume = action.volume;
+          if (dispatcher) dispatcher.setVolume(action.volume);
+          break;
+
+        case "TOGGLE_PLAY":
+          togglePlay();
+          break;
+
+        default:
+          console.log("WS action received:", action);
+      }
+    });
   });
 
-  ws.on("message", (message) => {
-    const action = JSON.parse(message);
-
-    switch (action.type) {
-      case "CONNECT_TO_CHANNEL":
-        connectToVoiceChannel(action.channelId);
-        break;
-
-      case "DISCONNECT":
-        disconnect();
-        break;
-
-      case "PLAY_SOUND":
-        playSound(action.sound);
-        break;
-
-      case "PLAY_URL":
-        playUrl(action.url);
-        break;
-
-      case "SET_VOLUME":
-        state.volume = action.volume;
-        if (dispatcher) dispatcher.setVolume(action.volume);
-        break;
-
-      case "TOGGLE_PLAY":
-        togglePlay();
-        break;
-
-      default:
-        console.log("WS action received:", action);
-    }
-  });
-});
-
-discordClient.on("ready", () => {
   state = {
     ...state,
     online: true,
@@ -198,8 +199,8 @@ discordClient.on("ready", () => {
       }))
       .sort((a, b) => (a.name < b.name ? -1 : 1)),
   };
+
+  console.log(`Listening on :${port}`);
 });
 
 discordClient.login(process.env["BOT_TOKEN"]);
-
-console.log(`Listening on :${port}`);
